86b23669a5b0d04fc743007e13b17f08909a07b3
Richard Abdill <rabdill@users.noreply.github.com>
2026-07-28 17:18:26 -0400

    Pull enum definitions into slot profile
    When we make a page that says datasetProcessingLevel can be one of several values, this tells the user what the values are.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ just gen-doc
 just testdoc
 ```
 
-http://127.0.0.1:8000/NAMHub/elements/datasetProcessingLevel/
+http://127.0.0.1:8000/nam-hub-models/elements/datasetProcessingLevel/
 
 
 *This project uses templates pulled from [linkml-project-copier](https://github.com/linkml/linkml-project-copier).*
--- config.public.mk
+++ config.public.mk
@@ -17,7 +17,7 @@ LINKML_SCHEMA_SOURCE_DIR="portal_schemas"
 LINKML_GENERATORS_CONFIG_YAML=config.yaml
 
 ## pass args if gendoc ignores config.yaml (i.e. --no-mergeimports)
-LINKML_GENERATORS_DOC_ARGS=
+LINKML_GENERATORS_DOC_ARGS="--template-directory docs/templates-linkml"
 
 ## pass args to workaround genowl rdfs config bug (linkml#1453)
 ##   (i.e. --no-type-objects --no-metaclasses --metadata-profile=rdfs)
